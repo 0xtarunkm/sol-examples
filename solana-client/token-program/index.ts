524829bd42031a00b9cefc4574b40035517ac427
tarunclub <tarunkm__@outlook.com>
2024-09-23 15:50:51 +0530

    fix: removed node_modules

--- solana-client/token-program/index.ts
+++ solana-client/token-program/index.ts
@@ -1,130 +1,130 @@
 import {
-    createMint,
-    getMint,
-    getOrCreateAssociatedTokenAccount,
-    mintTo,
-    transfer,
+  createMint,
+  getMint,
+  getOrCreateAssociatedTokenAccount,
+  mintTo,
+  transfer,
 } from '@solana/spl-token';
 import { Connection, Keypair, PublicKey, clusterApiUrl } from '@solana/web3.js';
-import "dotenv/config";
-import {getKeypairFromEnvironment} from "@solana-developers/helpers";
+import 'dotenv/config';
+import { getKeypairFromEnvironment } from '@solana-developers/helpers';
 
 async function createNewMint(
-    connection: Connection,
-    payer: Keypair,
-    mintAuthority: PublicKey,
-    freezeAuthority: PublicKey,
-    decimal: number
+  connection: Connection,
+  payer: Keypair,
+  mintAuthority: PublicKey,
+  freezeAuthority: PublicKey,
+  decimal: number
 ) {
-    const tokenMint = await createMint(
-        connection,
-        payer,
-        mintAuthority,
-        freezeAuthority,
-        decimal
-    );
+  const tokenMint = await createMint(
+    connection,
+    payer,
+    mintAuthority,
+    freezeAuthority,
+    decimal
+  );
 
-    console.log(
-        `Token Mint: https://explorer.solana.com/address/${tokenMint}?cluster=devnet`
-    );
+  console.log(
+    `Token Mint: https://explorer.solana.com/address/${tokenMint}?cluster=devnet`
+  );
 
-    return tokenMint;
+  return tokenMint;
 }
 
 async function createTokenAccount(
-    connection: Connection,
-    payer: Keypair,
-    mint: PublicKey,
-    owner: PublicKey
+  connection: Connection,
+  payer: Keypair,
+  mint: PublicKey,
+  owner: PublicKey
 ) {
-    const tokenAccount = await getOrCreateAssociatedTokenAccount(
-        connection,
-        payer,
-        mint,
-        owner
-    );
+  const tokenAccount = await getOrCreateAssociatedTokenAccount(
+    connection,
+    payer,
+    mint,
+    owner
+  );
 
-    console.log(
-        `Token account: https://explorer.solana.com/address/${tokenAccount.address}?cluster=devnet`
-    );
+  console.log(
+    `Token account: https://explorer.solana.com/address/${tokenAccount.address}?cluster=devnet`
+  );
 
-    return tokenAccount;
+  return tokenAccount;
 }
 
 async function mintToken(
-    connection: Connection,
-    payer: Keypair,
-    mint: PublicKey,
-    destination: PublicKey,
-    authority: Keypair,
-    amount: number
+  connection: Connection,
+  payer: Keypair,
+  mint: PublicKey,
+  destination: PublicKey,
+  authority: Keypair,
+  amount: number
 ) {
-    const transactionSignature = await mintTo(
-        connection,
-        payer,
-        mint,
-        destination,
-        authority,
-        amount
-    );
+  const transactionSignature = await mintTo(
+    connection,
+    payer,
+    mint,
+    destination,
+    authority,
+    amount
+  );
 
-    console.log(
-        `Mint Token Transaction: https://explorer.solana.com/tx/${transactionSignature}?cluster=devnet`
-    );
+  console.log(
+    `Mint Token Transaction: https://explorer.solana.com/tx/${transactionSignature}?cluster=devnet`
+  );
 }
 
 async function transferTokens(
-    connection: Connection,
-    payer: Keypair,
-    source: PublicKey,
-    destination: PublicKey,
-    owner: Keypair,
-    amount: number
+  connection: Connection,
+  payer: Keypair,
+  source: PublicKey,
+  destination: PublicKey,
+  owner: Keypair,
+  amount: number
 ) {
-    const transactionSignature = await transfer(
-        connection,
-        payer,
-        source,
-        destination,
-        owner,
-        amount
-    );
+  const transactionSignature = await transfer(
+    connection,
+    payer,
+    source,
+    destination,
+    owner,
+    amount
+  );
 
-    console.log(
-        `Transfer Transaction: https://explorer.solana.com/tx/${transactionSignature}?cluster=devnet`
-    );
+  console.log(
+    `Transfer Transaction: https://explorer.solana.com/tx/${transactionSignature}?cluster=devnet`
+  );
 }
 
 async function main() {
-    const connection = new Connection(clusterApiUrl('devnet'), 'confirmed');
+  const connection = new Connection(clusterApiUrl('devnet'), 'confirmed');
 
-    const keypair = getKeypairFromEnvironment("SECRET_KEY");
+  const keypair = getKeypairFromEnvironment('SECRET_KEY');
 
-    const mint = await createNewMint(
-        connection,
-        keypair,
-        keypair.publicKey,
-        keypair.publicKey,
-        9
-    );
+  const mint = await createNewMint(
+    connection,
+    keypair,
+    keypair.publicKey,
+    keypair.publicKey,
+    9
+  );
 
-    const tokenAccount = await createTokenAccount(
-        connection,
-        keypair,
-        mint,
-        keypair.publicKey
-    );
+  const tokenAccount = await createTokenAccount(
+    connection,
+    keypair,
+    mint,
+    keypair.publicKey
+  );
 
-    console.log(tokenAccount.address.toBase58());
+  console.log(tokenAccount.address.toBase58());
 
-    await mintToken(
-        connection,
-        keypair,
-        mint,
-        tokenAccount.address,
-        keypair,
-        100 * 10 ** 9
-    );
+  await mintToken(
+    connection,
+    keypair,
+    mint,
+    tokenAccount.address,
+    keypair,
+    100 * 10 ** 9
+  );
 }
 
 main();
